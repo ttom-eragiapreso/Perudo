374b3809f4990da1cd46acadea78dd22f5e891c0
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-09-01 18:59:10 +0200

    Roll btn only clickable once, result is stored as array

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('roll'); 
+let isBtnClickable = true;
 const result1 = document.querySelector('.die-1');
 const result2 = document.querySelector('.die-2');
 const result3 = document.querySelector('.die-3');
@@ -6,23 +7,33 @@ const result4 = document.querySelector('.die-4');
 const result5 = document.querySelector('.die-5');
 
 const randomNumberGenerator = () => {
-    let randomNumer1 = Math.floor(Math.random()* 6 + 1);
-    let randomNumer2 = Math.floor(Math.random()* 6 + 1);
-    let randomNumer3 = Math.floor(Math.random()* 6 + 1);
-    let randomNumer4 = Math.floor(Math.random()* 6 + 1);
-    let randomNumer5 = Math.floor(Math.random()* 6 + 1);
-    result1.textContent = randomNumer1;
-    result2.textContent = randomNumer2;
-    result3.textContent = randomNumer3;
-    result4.textContent = randomNumer4;
-    result5.textContent = randomNumer5;
-    let resultsArray = [randomNumer1, randomNumer2, randomNumer3, randomNumer4, randomNumer5]
-    localStorage.setItem('Result Player One', JSON.stringify(resultsArray));
-    
+  console.log(isBtnClickable);
+    if(isBtnClickable){
+      isBtnClickable = false;
+      let randomNumber1 = Math.floor(Math.random()* 6 + 1);
+      let randomNumber2 = Math.floor(Math.random()* 6 + 1);
+      let randomNumber3 = Math.floor(Math.random()* 6 + 1);
+      let randomNumber4 = Math.floor(Math.random()* 6 + 1);
+      let randomNumber5 = Math.floor(Math.random()* 6 + 1);
+      result1.textContent = randomNumber1;
+      result2.textContent = randomNumber2;
+      result3.textContent = randomNumber3;
+      result4.textContent = randomNumber4;
+      result5.textContent = randomNumber5;
+      let resultsArray = [randomNumber1, randomNumber2, randomNumber3, randomNumber4, randomNumber5]
+      if(localStorage.getItem('Result Player One') === null){
+        localStorage.setItem('Result Player One', JSON.stringify(resultsArray));
+      }
+      else {
+        return
+      };
+    }
+    else {
+      console.error('Wait for next turn');
+    }
 };
 
 btn.addEventListener('click', randomNumberGenerator);
-
 // Other types of mouve events 
 // btn.addEventListener('dblclick', randomNumberGenerator);
 // btn.addEventListener('mousedown', randomNumberGenerator);  as soon as the mouse is down
